2f2e4a79fbcaa2ae3cdf10d703bcda881493ef54
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-14 14:36:05 -0300

    fazendo o desafio do erro404

--- vite-project/src/Rounter.jsx
+++ vite-project/src/Rounter.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Inicial, SobreNos } from "./pages";
+import { Erro404, Inicial, SobreNos } from "./pages";
 import { LayoutPadrao } from "./layouts";
 
 const Rounter = () => {
@@ -8,6 +8,7 @@ const Rounter = () => {
       <Route path="/" element={<LayoutPadrao />}>
         <Route index element={<Inicial />} />
         <Route path="sobre-nos" element={<SobreNos />} />
+        <Route path="*" element={<Erro404 />} />
       </Route>
     </Routes>
   );
